5b71d4fb72273ff24d89e3e691a438fb60911743
Lakshya Vohra <lakshyavohra09@gmail.com>
2025-03-20 16:27:26 +0530

    wall following algorithm

--- algorithm.py
+++ algorithm.py
@@ -1,35 +1,36 @@
 from motion_pid import *
+DURATION = 0.3
 def run():
     choices = move_with_pid_pulses()
     if choices == [1, 1, 1]:
         motors.stop_motors()
         rotate()
-        utime.sleep(0.1)
+        utime.sleep(DURATION)
         rotate()
-        utime.sleep(0.1)
+        utime.sleep(DURATION)
     elif (choices[0] == 0 and sum(choices)!=0):
         # utime.sleep_ms(250)
         motors.stop_motors()
         rotate(-60.25)
-        utime.sleep(0.1)
+        utime.sleep(DURATION)
     elif choices == [0, 1, 0]:
         # utime.sleep_ms(250)
         motors.stop_motors()
         rotate(-60.25)
-        utime.sleep(0.1)
+        utime.sleep(DURATION)
          # Stop movement
     elif choices == [0, 1, 1]:
         # utime.sleep_ms(250)
         motors.stop_motors()
         rotate(-60.25)
-        utime.sleep(0.1)
+        utime.sleep(DURATION)
     elif choices == [1, 1, 0]:
         # utime.sleep_ms(250)
         motors.stop_motors()
         rotate()
-        utime.sleep(0.1)
+        utime.sleep(DURATION)
     elif choices == [1, 0, 0]:
         # utime.sleep_ms(250)
         motors.stop_motors()
         rotate()
-        utime.sleep(0.1)
+        utime.sleep(DURATION)
